0b362720e6af5a389275ec47631de1ca8a392345
Krithi <krithiur@test.com>
2019-01-30 10:16:50 +0530

    first commit

---
+++ ship-wreck/src/main/java/com/mindtree/ship_wreck/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mindtree.ship_wreck.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "My Home page";
+	}
+}
